528a98c59df37953966a376869e13a67afd780da
Sébastien Barbier <contact@sebastienbarbier.com>
2021-10-08 17:10:47 +0200

    Remove requirement.txt install from legacy code.

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
-# Install python dependancies
-pip3 install -r requirements.txt
-
 # If ./.env file exist, we export variables to current system to display later
 if [ -f .env ]; then
   export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
